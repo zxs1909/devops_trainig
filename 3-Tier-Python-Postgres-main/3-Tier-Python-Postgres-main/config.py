4cabd3c305cfba69e60b71dcd490b7a8ae5f7300
zxs1909 <zafar.devop@gmail.com>
2024-10-30 23:48:25 +0530

    remove the project

--- 3-Tier-Python-Postgres-main/3-Tier-Python-Postgres-main/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'postgresql://root:root@localhost/my_database'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
